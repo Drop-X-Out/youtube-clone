c11d557f053e0e5b7693abacdf52d92c89d1c70b
Drop-X-Out <officialgameofcodes@gmail.com>
2023-10-11 12:36:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home'
+import { BrowserRouter, Routes ,Route} from 'react-router-dom'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/search' element={<Search/>}/>
+    <Route path='/watch/:id' element={<Watch/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
